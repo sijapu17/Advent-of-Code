0f97244e5eead302ead6893131ff40d15344e31e
sijapu17 <simon.purkess@hotmail.com>
2019-12-29 19:36:14 +0000

    Added Day 14 Part 2

--- 2019/2019-14.py
+++ 2019/2019-14.py
@@ -28,18 +28,48 @@ def nextReaction(inventory): #Finds a chemical that needs to be produced, only r
             return(k)
     return('ORE')
 
-def solveA(recipes): #Determine amount of ORE required to produce 1 FUEL, working backwards
+def oreForNFuel(recipes,n): #Determine amount of ORE required to produce n FUEL, working backwards
     inventory=defaultdict(int) #Positive count represents outstanding chemicals, negative count represents spares
-    inventory['FUEL']=1 #Request 1 FUEL
+    inventory['FUEL']=n #Request 1 FUEL
     prod=nextReaction(inventory)
     while prod!='ORE':
         recipe=recipes[prod]
-        inventory[prod]-=recipe['quantity'] #Subtract product inventory by recipe quantity
-        for k,v in recipe['ingredients'].items(): #Add 
-            inventory[k]+=v
+        mult=(inventory[prod] + (recipe['quantity']-1)) // recipe['quantity'] #Number of times to run the recipe so prod inventory is 0 or lower
+        inventory[prod]-=(mult*recipe['quantity']) #Subtract product inventory by quantity of products produced
+        for k,v in recipe['ingredients'].items(): #Add ingredients used to product inventory
+            inventory[k]+=(mult*v)
         prod=nextReaction(inventory)
-    print(str(inventory))
     return(inventory['ORE'])
         
-retA=solveA(recipes)        
-        
+retA=oreForNFuel(recipes,1)
+
+def fuelForTrillionOre(recipes): #Determine amount of fuel that can be produced from 1 trillion ore, using binary search
+    trillion=1000000000000 #1 trillion
+    lBound=trillion//oreForNFuel(recipes,1) #At least this much fuel can be produced by repeating the 1-fuel process, but this leaves many leftovers
+    uBound=lBound*2
+    while True:
+        if oreForNFuel(recipes,uBound)<trillion: #If upper bound is too low, double it and try again
+            uBound*=2
+        else:
+            break        
+    while lBound+1<uBound: #Once range is established, test the midpoint and then halve the range until only two values remain
+        print('Range: ('+str(lBound)+', '+str(uBound)+')')
+        mid=(lBound+uBound)//2
+        ore=oreForNFuel(recipes,mid)
+        print(str(mid)+' fuel from '+str(ore)+' ore')
+        if ore>trillion: #If more than a trillion ore required to produce fuel, set upper bound to midpoint
+            uBound=mid
+        elif ore<=trillion: #If less than a trillion ore required to produce fuel, set lower bound to midpoint
+            lBound=mid
+    #Test final 2 values to find which is correct
+    print('Range: ('+str(lBound)+', '+str(uBound)+')')
+    lOre=oreForNFuel(recipes,lBound)
+    uOre=oreForNFuel(recipes,uBound)
+    print(str(lBound)+' fuel from '+str(lOre)+' ore')
+    print(str(uBound)+' fuel from '+str(uOre)+' ore')
+    if uOre>trillion: #Test whether upper bound is past a trillion or not
+        return(lBound)
+    else:
+        return(uBound)
+             
+retB=fuelForTrillionOre(recipes)
